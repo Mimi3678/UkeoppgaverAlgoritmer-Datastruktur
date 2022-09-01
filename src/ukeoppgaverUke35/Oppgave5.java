a38304cf5f16ae48ea8ad1953a4635bbba6fd8d7
Mimi3678 <89571180+Mimi3678@users.noreply.github.com>
2022-09-01 11:51:28 +0200

    mappe til ukesoppgaver uke35, oppgave 5

---
+++ src/ukeoppgaverUke35/Oppgave5.java
@@ -0,0 +1,33 @@
+package ukeoppgaverUke35;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+//Seksjon 1.2.1
+public class Oppgave5 {
+    public static void main(String[] args){
+
+        //eksempel med en tabell
+       int [] a = {1,2,3,4,5,6,7,8,9,10};
+       for (int k:a) System.out.print(k + " ");
+
+       System.out.println("\n");
+
+       //eksempel med en liste
+        List<Character> liste = new LinkedList<>();
+        liste.add('A'); liste.add('B'); liste.add('C');
+
+        for (Character c : liste) System.out.print(c + " ");
+
+        System.out.println("\n");
+
+        //eksempel med en tabell og mengde
+        int[] array = {5,9,2,4,7,10,8,1,3,6};
+        Set<Integer> s = new TreeSet<>();
+
+        for (int k:array) s.add(k);
+        for (int k:s) System.out.print(k + " ");
+    }
+}
